77ac17e56e4eda0ba20e84bb129cf14d72ca0134
ecila7290 <jihoon.kim89@gmail.com>
2021-01-12 22:47:04 +0900

    Add: create user and get user

--- server.go
+++ server.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"net/http"
+	"strconv"
+
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
@@ -13,10 +16,39 @@ type user struct {
 var users = map[int]*user{}
 var seq = 1
 
+func createUser(c echo.Context) error {
+	u := &user{
+		ID: seq,
+	}
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	users[u.ID] = u
+	seq++
+	return c.JSON(http.StatusCreated, u)
+}
+
+func getUser(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	return c.JSON(http.StatusOK, users[id])
+}
+
+func getAllUsers(c echo.Context) error {
+	return c.JSON(http.StatusOK, users)
+}
+
 func main() {
 	e := echo.New()
+
+	// middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
+	// routes
+	e.GET("/users", getAllUsers)
+	e.POST("/users", createUser)
+	e.GET("/users/:id", getUser)
+
+	// start server
 	e.Logger.Fatal(e.Start(":1323"))
 }
